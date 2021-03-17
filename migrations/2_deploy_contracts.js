e47f85e3f6fddc6e991716730bc22429fe6324cf
cryptoleedle <cryptoleedle@protonmail.com>
2021-03-17 04:52:55 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 const Factory = artifacts.require('uniswapv2/UniswapV2Factory.sol');
 const Router = artifacts.require('uniswapv2/UniswapV2Router02.sol');
+const XERC20 = artifacts.require('@openzeppelin/contracts/token/XERC20');
 const WETH = artifacts.require('WETH.sol');
 const MockERC20 = artifacts.require('MockERC20.sol');
 const SushiToken = artifacts.require('SushiToken.sol') 
@@ -16,6 +17,13 @@ module.exports = async function(deployer, _network, addresses) {
   };
   await deployer.deploy(WETH);
   const weth = await WETH.deployed();
+
+  await deployer.deploy(XERC20, 'TEST1', 'TST1');
+  const erc201 = await ERC20.deploy();
+
+  await deployer.deploy(XERC20, 'TEST2', 'TST2');
+  const erc202 = await ERC20.deploy();
+
   const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
   const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
 
@@ -35,7 +43,7 @@ module.exports = async function(deployer, _network, addresses) {
     admin,
     web3.utils.toWei('100'),
     1,
-    1
+    10
   );
   const masterChef = await MasterChef.deployed();
   await sushiToken.transferOwnership(masterChef.address);
