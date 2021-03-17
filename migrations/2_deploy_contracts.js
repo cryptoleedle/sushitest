ec0139ea2e11a685562a66b80fac96926c2cfa9d
cryptoleedle <cryptoleedle@protonmail.com>
2021-03-17 05:07:22 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,117 +9,57 @@ const SushiBar = artifacts.require('SushiBar.sol');
 const SushiMaker = artifacts.require('SushiMaker.sol');
 const Migrator = artifacts.require('Migrator.sol');
 
-
 module.exports = async function(deployer, _network, accounts) {
   if (_network == 'ropsten') {
     const admin = '0x420Dab420c5Fd0C067C4b8007448F0256abe2006';
-    //const weth = '0x0a180a76e4466bf68a7f86fb029bed3cccfaaac5';
-    await deployer.deploy(WETH);
-    const weth = await WETH.deployed();
-
-    // await deployer.deploy(ERC20, 'TEST', 'SOUP');
-    // const erc20 = await ERC20.deployed();
-
-    const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
-    const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
-    // console.log(tokenA);
-    // console.log(tokenB);
-    
-    await deployer.deploy(Factory, admin);
-    const factory = await Factory.deployed();
-    await factory.createPair(weth.address, tokenA.address);
-    await factory.createPair(weth.address, tokenB.address);
-    await deployer.deploy(Router, factory.address, weth.address);
-    const router = await Router.deployed();
-
-    // await tokenA.approve(router.address, web3.utils.toWei('1000'));
-    // await tokenB.approve(router.address, web3.utils.toWei('1000'));
-
-    await deployer.deploy(SushiToken);
-    const sushiToken = await SushiToken.deployed();
-
-    await deployer.deploy(
-      MasterChef,
-      sushiToken.address,
-      admin,
-      web3.utils.toWei('100'),
-      1,
-      10000
-    );
-    const masterChef = await MasterChef.deployed();
-    //await masterChef.transferOwnership(admin);
-    await sushiToken.transferOwnership(masterChef.address);
-
-    await deployer.deploy(SushiBar, sushiToken.address);
-    const sushiBar = await SushiBar.deployed();
-
-    await deployer.deploy(
-      SushiMaker,
-      factory.address, 
-      sushiBar.address, 
-      sushiToken.address, 
-      weth.address
-    );
-    const sushiMaker = await SushiMaker.deployed();
-    await factory.setFeeTo(sushiMaker.address);
-
-
   } else {
-
-
     const admin = accounts[0];
-    //const weth = '0x0a180a76e4466bf68a7f86fb029bed3cccfaaac5';
-    await deployer.deploy(WETH);
-    const weth = await WETH.deployed();
-    const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
-    const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
-    //console.log(tokenA);
-    //console.log(tokenB);
-    
-    await deployer.deploy(Factory, admin);
-    const factory = await Factory.deployed();
-    await factory.createPair(weth.address, tokenA.address);
-    await factory.createPair(weth.address, tokenB.address);
-    await deployer.deploy(Router, factory.address, weth.address);
-    const router = await Router.deployed();
+  };
+  await deployer.deploy(WETH);
+  const weth = await WETH.deployed();
 
-    //await tokenA.approve(router.address, web3.utils.toWei('1000'));
-    //await tokenB.approve(router.address, web3.utils.toWei('1000'));
+  // await deployer.deploy(XERC20, 'TEST1', 'TST1');
+  // const erc201 = await ERC20.deploy();
 
-    await deployer.deploy(SushiToken);
-    const sushiToken = await SushiToken.deployed();
+  // await deployer.deploy(XERC20, 'TEST2', 'TST2');
+  // const erc202 = await ERC20.deploy();
 
-    await deployer.deploy(
-      MasterChef,
-      sushiToken.address,
-      admin,
-      web3.utils.toWei('100'),
-      1,
-      10
-    );
-    const masterChef = await MasterChef.deployed();
-    //await masterChef.transferOwnership(admin);
-    await sushiToken.transferOwnership(masterChef.address);
+  const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
+  const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
 
-    await deployer.deploy(SushiBar, sushiToken.address);
-    const sushiBar = await SushiBar.deployed();
+  await deployer.deploy(Factory, admin);
+  const factory = await Factory.deployed();
+  await factory.createPair(weth.address, tokenA.address);
+  await factory.createPair(weth.address, tokenB.address);
+  await deployer.deploy(Router, factory.address, weth.address);
+  const router = await Router.deployed();
 
-    await deployer.deploy(
-      SushiMaker,
-      factory.address, 
-      sushiBar.address, 
-      sushiToken.address, 
-      weth.address
-    );
-    const sushiMaker = await SushiMaker.deployed();
-    await factory.setFeeTo(sushiMaker.address);
-  }
+  await deployer.deploy(SushiToken);
+  const sushiToken = await SushiToken.deployed();
 
-  //await router.addLiquidity(tokenA.address, tokenB.address, web3.utils.toWei('900'), web3.utils.toWei('900'), web3.utils.toWei('800'), web3.utils.toWei('800'), admin, 1715698160);
-  
-  //const lpaddress = await UniswapV2Library.pairFor(factory.address, tokenA.address, tokenB.address);
-  //await masterChef.add(0, lpaddress, 1);
-  //await masterChef.deposit(0, web3.utils.toWei('900'));
+  await deployer.deploy(
+    MasterChef,
+    sushiToken.address,
+    admin,
+    web3.utils.toWei('100'),
+    1,
+    10
+  );
+  const masterChef = await MasterChef.deployed();
+  await sushiToken.transferOwnership(masterChef.address);
+
+  await deployer.deploy(SushiBar, sushiToken.address);
+  const sushiBar = await SushiBar.deployed();
+
+  await deployer.deploy(
+    SushiMaker,
+    factory.address, 
+    sushiBar.address, 
+    sushiToken.address, 
+    weth.address
+  );
+  const sushiMaker = await SushiMaker.deployed();
+  await factory.setFeeTo(sushiMaker.address);
 
   // await deployer.deploy(
   //   Migrator,
@@ -129,66 +69,3 @@ module.exports = async function(deployer, _network, accounts) {
   //   1
   // );
 };
-
-
-
-// module.exports = async function(deployer, _network, addresses) {
-//   if (_network == 'ropsten') {
-//     const admin = '0x420Dab420c5Fd0C067C4b8007448F0256abe2006';
-//   } else {
-//     const [admin, _] = addresses;
-//   };
-//   await deployer.deploy(WETH);
-//   const weth = await WETH.deployed();
-
-//   // await deployer.deploy(XERC20, 'TEST1', 'TST1');
-//   // const erc201 = await ERC20.deploy();
-
-//   // await deployer.deploy(XERC20, 'TEST2', 'TST2');
-//   // const erc202 = await ERC20.deploy();
-
-//   const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
-//   const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
-
-//   await deployer.deploy(Factory, admin);
-//   const factory = await Factory.deployed();
-//   await factory.createPair(weth.address, tokenA.address);
-//   await factory.createPair(weth.address, tokenB.address);
-//   await deployer.deploy(Router, factory.address, weth.address);
-//   const router = await Router.deployed();
-
-//   await deployer.deploy(SushiToken);
-//   const sushiToken = await SushiToken.deployed();
-
-//   await deployer.deploy(
-//     MasterChef,
-//     sushiToken.address,
-//     admin,
-//     web3.utils.toWei('100'),
-//     1,
-//     10
-//   );
-//   const masterChef = await MasterChef.deployed();
-//   await sushiToken.transferOwnership(masterChef.address);
-
-//   await deployer.deploy(SushiBar, sushiToken.address);
-//   const sushiBar = await SushiBar.deployed();
-
-//   await deployer.deploy(
-//     SushiMaker,
-//     factory.address, 
-//     sushiBar.address, 
-//     sushiToken.address, 
-//     weth.address
-//   );
-//   const sushiMaker = await SushiMaker.deployed();
-//   await factory.setFeeTo(sushiMaker.address);
-
-//   // await deployer.deploy(
-//   //   Migrator,
-//   //   masterChef.address,
-//   //   '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-//   //   factory.address,
-//   //   1
-//   // );
-// };
